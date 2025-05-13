9debc71f5b9a23be583d65105eebb3e92ad61f3e
osr81 <osr81@CARIN_SED>
2025-05-13 07:39:10 -0600

    Agregar archivos de proyecto.

---
+++ git_hub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_hub
+{
+    internal class Program
+        //OSCAR SEDANO RIVERA//
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
